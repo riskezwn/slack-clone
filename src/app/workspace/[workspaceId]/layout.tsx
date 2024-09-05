3aea54b2ffbb09124c5ebc58dd43afed6a0a9753
Enrique García <enriquegarciagasc@gmail.com>
2024-09-05 17:44:03 +0200

    add layout and toolbar to workspaceId page

---
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Toolbar } from './toolbar';
+
+interface WorkspaceIdLayoutProps {
+  children: React.ReactNode;
+}
+
+const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
+  return (
+    <div className="h-full">
+      <Toolbar />
+      {children}
+    </div>
+  );
+};
+
+export default WorkspaceIdLayout;
---
+++ src/app/workspace/[workspaceId]/toolbar.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { Info, Search } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+import { useGetWorkspace } from '@/features/workspaces/api/use-get-workspace';
+import { useWorkspaceId } from '@/hooks/use-workspace-id';
+
+export const Toolbar = () => {
+  const workspaceId = useWorkspaceId();
+
+  const { data } = useGetWorkspace({ id: workspaceId });
+  return (
+    <nav className="flex h-10 items-center justify-between bg-[#481349] p-1.5">
+      <div className="flex-1" />
+      <div className="max-[642px] min-w-[280px] shrink grow-[2]">
+        <Button
+          size="sm"
+          className="h-7 w-full justify-start bg-accent/25 px-2 hover:bg-accent/25"
+        >
+          <Search className="mr-2 size-4 text-white" />
+          <span className="text-xs text-white">Search {data?.name}</span>
+        </Button>
+      </div>
+      <div className="ml-auto flex flex-1 items-center justify-end">
+        <Button variant="transparent" size="iconSm">
+          <Info className="size-5 text-white" />
+        </Button>
+      </div>
+    </nav>
+  );
+};
