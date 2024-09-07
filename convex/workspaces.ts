ee307db119e1c5a6929f48c51cd50c1800b7149a
Enrique García <enriquegarciagasc@gmail.com>
2024-09-07 12:42:48 +0200

    add members table and modify workspaces endpoints

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -5,10 +5,42 @@ import { query, mutation } from './_generated/server';
 
 const TABLE = 'workspaces';
 
+const generateCode = () => {
+  const code = Array.from(
+    {
+      length: 6,
+    },
+    () => '0123456789abcdefghijklmnopqrstuvwxyz'[Math.floor(Math.random() * 36)],
+  ).join('');
+
+  return code;
+};
+
 export const get = query({
   args: {},
   handler: async (ctx) => {
-    return await ctx.db.query(TABLE).collect();
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) return [];
+
+    const members = await ctx.db
+      .query('members')
+      .withIndex('by_user_id', (q) => q.eq('userId', userId))
+      .collect();
+
+    const workspacesId = members.map((member) => member.workspaceId);
+
+    const workspaces = [];
+
+    for (const workspaceId of workspacesId) {
+      const workspace = await ctx.db.get(workspaceId);
+
+      if (workspace) {
+        workspaces.push(workspace);
+      }
+    }
+
+    return workspaces;
   },
 });
 
@@ -20,6 +52,15 @@ export const getById = query({
     const userId = await getAuthUserId(ctx);
 
     if (!userId) throw new Error('Unauthorized');
+
+    const member = await ctx.db
+      .query('members')
+      .withIndex('by_workspace_id_by_user_id', (q) =>
+        q.eq('workspaceId', args.id).eq('userId', userId),
+      )
+      .unique();
+
+    if (!member) return null;
 
     return await ctx.db.get(args.id);
   },
@@ -34,12 +75,18 @@ export const create = mutation({
 
     if (!userId) throw new Error('Unauthorized');
 
-    const joinCode = '123456'; // TODO: Create a proper method
+    const joinCode = generateCode();
 
     const workspaceId = await ctx.db.insert(TABLE, {
       name: args.name,
       userId,
       joinCode,
+    });
+
+    await ctx.db.insert('members', {
+      userId,
+      workspaceId,
+      role: 'admin',
     });
 
     return workspaceId;
